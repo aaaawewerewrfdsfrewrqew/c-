4cab10f8683b19377ef75e76a66eef229cb6472b
aaaawewerewrfdsfrewrqew <maks212pod@gmail.com>
2025-09-10 19:17:50 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "\"Як умру, то поховайте\n"
+        "Мене на могилі,\n"
+        "Серед степу широкого,\n"
+        "На Вкраїні милій...\"\n\t- Тарас Шевченко\n\n";
+
+    cout << "Every hunter wants to know where the pheasant sits.\n";
+    cout << "\tRed\n";
+    cout << "\t\tOrange\n";
+    cout << "\t\t\tYellow\n";
+    cout << "\t\t\t\tGreen\n";
+    cout << "\t\t\t\t\tBlue\n";
+    cout << "\t\t\t\t\t\tIndigo\n";
+    cout << "\t\t\t\t\t\t\tViolet\n\n";
+
+    cout << "Продам процесор.\n";
+    cout << "Модель: AMD Ryzen 5 5600.\n";
+    cout << "Стан: майже новий.\n";
+    cout << "Ціна: 3 000 грн.\n";
+    cout << "Тел.: 222-22-22\n\n";
+
+    return 0;
+}
